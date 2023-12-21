76e3c6333eb1b69f70604c0cd9d0a790b59bfdc3
drew-chase <dcmanproductions@gmail.com>
2023-12-21 09:58:55 -0500

    Add ResourceItemBase class for monitoring system resources

---
+++ SysMonitor.NET/ResourceItemBase.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace SysMonitor.NET;
+
+public abstract class ResourceItemBase<T> where T : struct, IComparable
+{
+    [JsonProperty("name")]
+    public string Name { get; private set; }
+    [JsonProperty("max")]
+    public T MaxValue { get; private set; }
+    [JsonProperty("min")]
+    public T MinValue { get; private set; }
+    [JsonProperty("current")]
+    public T CurrentValue { get; private set; }
+    protected ResourceItemBase(string name, T maxValue, T minValue)
+    {
+        Name = name;
+        MaxValue = maxValue;
+        MinValue = minValue;
+    }
+    public async Task<T> GetSystemUsageAsync() => await Task.Run(GetSystemUsage);
+    public async Task<T> GetApplicationUsageAsync() => await Task.Run(GetApplicationUsage);
+    public abstract T GetSystemUsage();
+    public abstract T GetApplicationUsage();
+
+    public JObject ToJson()
+    {
+        CurrentValue = GetSystemUsage();
+        return JObject.FromObject(this);
+    }
+    public override string ToString()
+    {
+        return JsonConvert.SerializeObject(ToJson());
+    }
+}
