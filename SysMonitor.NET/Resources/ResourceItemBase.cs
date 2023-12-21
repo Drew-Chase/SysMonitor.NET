739d891b94f452c2801aa9c73dfc02f43bd27188
drew-chase <dcmanproductions@gmail.com>
2023-12-21 14:26:54 -0500

    Add Update and UpdateAsync methods to ResourceItemBase

--- SysMonitor.NET/Resources/ResourceItemBase.cs
+++ SysMonitor.NET/Resources/ResourceItemBase.cs
@@ -5,17 +5,28 @@ namespace SysMonitor.NET.Resources;
 
 public abstract class ResourceItemBase<T> where T : struct, IComparable
 {
+    [JsonIgnore]
+    public IResourceResult Result { get; protected set; }
     [JsonProperty("name")]
-    public string Name { get; private set; }
+    public string Name { get; protected set; }
     [JsonProperty("max")]
-    public T MaxValue { get; private set; }
+    public T MaxValue { get; protected set; }
     [JsonProperty("min")]
-    public T MinValue { get; private set; }
+    public T MinValue { get; protected set; }
     protected ResourceItemBase(string name, T maxValue, T minValue)
     {
         Name = name;
         MaxValue = maxValue;
         MinValue = minValue;
+    }
+    public void Update()
+    {
+        GetApplicationUsage();
+        GetSystemUsage();
+    }
+    public async Task UpdateAsync()
+    {
+        await Task.WhenAll(GetApplicationUsageAsync(), GetSystemUsageAsync());
     }
     public async Task<T> GetSystemUsageAsync() => await Task.Run(GetSystemUsage);
     public async Task<T> GetApplicationUsageAsync() => await Task.Run(GetApplicationUsage);
