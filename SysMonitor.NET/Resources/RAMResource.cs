671948e5af39ff34a086cec96a27d4f6207fe6e0
drew-chase <dcmanproductions@gmail.com>
2023-12-21 14:26:39 -0500

    Add RAMResource class for monitoring RAM usage

---
+++ SysMonitor.NET/Resources/RAMResource.cs
@@ -0,0 +1,32 @@
+﻿using Hardware.Info;
+using System.Diagnostics;
+
+namespace SysMonitor.NET.Resources;
+
+internal class RAMResource : ResourceItemBase<ulong>
+{
+    readonly IHardwareInfo hardwareInfo = new HardwareInfo();
+    public RAMResource() : base("RAM", 0, 0)
+    {
+        hardwareInfo.RefreshMemoryList();
+        hardwareInfo.MemoryList.ForEach(memory =>
+        {
+            MaxValue += memory.Capacity;
+        });
+        Result = new RAMResult() { Max = MaxValue, Min = MinValue };
+    }
+
+    public override ulong GetApplicationUsage()
+    {
+        ulong value = (ulong)Process.GetCurrentProcess().WorkingSet64;
+        ((RAMResult)Result).Application = value;
+        return value;
+    }
+
+    public override ulong GetSystemUsage()
+    {
+        ulong value = (ulong)Process.GetProcesses().Sum(p => p.WorkingSet64);
+        ((RAMResult)Result).System = value;
+        return value;
+    }
+}
