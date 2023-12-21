48422139bde157112d4c2026a71b947c76cd0e4d
drew-chase <dcmanproductions@gmail.com>
2023-12-21 16:18:35 -0500

    Add NetworkResource class

---
+++ SysMonitor.NET/Resources/NetworkResource.cs
@@ -0,0 +1,18 @@
+﻿namespace SysMonitor.NET.Resources;
+
+public class NetworkResource : ResourceItemBase<RWData>
+{
+    public NetworkResource() : base("Networking", new(ulong.MaxValue, ulong.MaxValue), new(0, 0))
+    {
+    }
+
+    public override RWData GetApplicationUsage()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override RWData GetSystemUsage()
+    {
+        throw new NotImplementedException();
+    }
+}
