57b2223b288034b06d02639ef9192313a35c626a
drew-chase <dcmanproductions@gmail.com>
2023-12-21 16:18:32 -0500

    Add DiskResource class for monitoring disk usage

---
+++ SysMonitor.NET/Resources/DiskResource.cs
@@ -0,0 +1,99 @@
+﻿using Newtonsoft.Json.Linq;
+using System.Diagnostics;
+
+namespace SysMonitor.NET.Resources;
+
+public class DiskResource : ResourceItemBase<RWData>
+{
+    public DiskResource() : base("Disk", new(ulong.MaxValue, ulong.MaxValue), new(0, 0))
+    {
+    }
+
+    public override RWData GetApplicationUsage()
+    {
+        return new(0, 0);
+    }
+
+    public override RWData GetSystemUsage()
+    {
+        string driveLetter = DriveInfo.GetDrives().First(i => i.DriveType.Equals(DriveType.Fixed)).Name;
+        RWData data = new(0, 0);
+
+        if (OperatingSystem.IsWindows())
+        {
+            PerformanceCounter readCounter = new("PhysicalDisk", "Disk Read Bytes/sec", "_Total");
+            PerformanceCounter writeCounter = new("PhysicalDisk", "Disk Write Bytes/sec", "_Total");
+            return new((ulong)(readCounter?.NextValue() ?? 0f), (ulong)(writeCounter?.NextValue() ?? 0L));
+        }
+        else if (OperatingSystem.IsLinux())
+        {
+            /*
+             * IOSTAT - Thirdparty software that needs to be installed!
+             * -d - Report the device utilization report
+             * -k - Display statistics in kilobytes per second instead of blocks per second.
+             * -x 1 - Also makes it only run once?
+             * -c 1 - Only runs once
+             * -o JSON - Output in JSON format
+             */
+            string command = $"iostat -d -k -x 1 -c 1 -o JSON";
+            ProcessStartInfo processInfo = new("bash", $"-c \"{command}\"")
+            {
+                RedirectStandardOutput = true,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
+            Process process = new() { StartInfo = processInfo };
+            process.BeginOutputReadLine();
+            process.BeginErrorReadLine();
+            string content = "";
+            process.OutputDataReceived += (sender, data) =>
+            {
+                if (!string.IsNullOrWhiteSpace(data.Data))
+                {
+                    content += data.Data;
+                }
+            };
+            process.Start();
+            process.WaitForExit();
+            JObject json = JObject.Parse(content);
+            long reads = 0;
+            long writes = 0;
+            if (json?["sysstat"]?["hosts"] is JArray hosts)
+            {
+                foreach (JObject host in hosts.Cast<JObject>())
+                {
+                    if (host?["statistics"] is JArray stats)
+                    {
+                        foreach (JObject stat in stats.Cast<JObject>())
+                        {
+                            if (stat?["disk"] is JArray disks)
+                            {
+                                foreach (JObject disk in disks.Cast<JObject>())
+                                {
+                                    if (disk?["name"]?.ToString().Equals(driveLetter) ?? false)
+                                    {
+                                        reads += (long)(disk?["rkB/s"]?.ToObject<double>() ?? 0) * 1024;
+                                        writes += (long)(disk?["wkB/s"]?.ToObject<double>() ?? 0) * 1024;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        else if (OperatingSystem.IsMacOS())
+        {
+            string command = $"iotop"; // TODO: REPLACE THIS
+            ProcessStartInfo processInfo = new("bash", $"-c \"{command}\"")
+            {
+                RedirectStandardOutput = true,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
+            Process process = new() { StartInfo = processInfo };
+            process.Start();
+        }
+        return (RWData)(Result = data);
+    }
+}
