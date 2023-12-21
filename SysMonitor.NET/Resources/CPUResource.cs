d2e7f16b5d1e23a9a62c5b878da53ae981a9191a
drew-chase <dcmanproductions@gmail.com>
2023-12-21 14:26:29 -0500

    Refactor CPUResource class to improve performance and accuracy

--- SysMonitor.NET/Resources/CPUResource.cs
+++ SysMonitor.NET/Resources/CPUResource.cs
@@ -2,49 +2,74 @@
 
 namespace SysMonitor.NET.Resources;
 
-public class CPUResource() : ResourceItemBase<double>("CPU", 1d, 0d)
+public class CPUResource : ResourceItemBase<double>
 {
+    public CPUResource() : base("CPU", 1d, 0d)
+    {
+        Result = new CPUResult() { Max = MaxValue, Min = MinValue };
+    }
     public override double GetApplicationUsage()
     {
-        throw new NotImplementedException();
+
+        long startTime = Environment.TickCount64;
+
+        TimeSpan startUsage = Process.GetCurrentProcess().TotalProcessorTime;
+
+        while ((Environment.TickCount64 - startTime) < 100)
+        {
+        }
+
+        TimeSpan currentUsage = Process.GetCurrentProcess().TotalProcessorTime;
+
+
+        double elapsedMilliseconds = Environment.TickCount64 - startTime;
+
+        double value = Math.Min((currentUsage - startUsage).TotalMilliseconds / (Environment.ProcessorCount * elapsedMilliseconds), 1);
+        ((CPUResult)Result).Application = value;
+        return value;
     }
 
     public override double GetSystemUsage()
     {
         long startTime = Environment.TickCount64;
-        Process[] startProcesses = Process.GetProcesses();
-        while ((Environment.TickCount64 - startTime) < 100) // Wait 100ms
+
+        Process[] processesBefore = Process.GetProcesses();
+
+        while ((Environment.TickCount64 - startTime) < 100)
         {
         }
 
-        Process[] endProcesses = Process.GetProcesses();
-        double startTotalProcessTime = startProcesses.Sum(p =>
-        {
-            try
-            {
-                return p.TotalProcessorTime.TotalMilliseconds;
-            }
-            catch (UnauthorizedAccessException)
-            {
-                return 0;
-            }
-        });
-        double endTotalProcessTime = endProcesses.Sum(p =>
-        {
-            try
-            {
-                return p.TotalProcessorTime.TotalMilliseconds;
-            }
-            catch (UnauthorizedAccessException)
-            {
-                return 0;
-            }
-        });
-        double totalProcessTime = endTotalProcessTime - startTotalProcessTime;
-        double totalSystemTime = Environment.TickCount64 - startTime;
-        double timePerCore = totalProcessTime / Environment.ProcessorCount * totalSystemTime;
-        double adjustedTimePerCore = timePerCore * 10;
-        return CurrentValue = Math.Max(Math.Min(adjustedTimePerCore, MaxValue), MinValue);
+        Process[] processesAfter = Process.GetProcesses();
 
+        double totalProcessTimeBefore = processesBefore.Sum(p =>
+        {
+            try
+            {
+                return p.TotalProcessorTime.TotalMilliseconds;
+            }
+            catch
+            {
+                return 0;
+            }
+        });
+
+        double totalProcessTimeAfter = processesAfter.Sum(p =>
+        {
+            try
+            {
+                return p.TotalProcessorTime.TotalMilliseconds;
+            }
+            catch
+            {
+                return 0;
+            }
+        });
+
+        double elapsedMilliseconds = Environment.TickCount64 - startTime;
+
+        double value = Math.Min((totalProcessTimeAfter - totalProcessTimeBefore) / ((Environment.ProcessorCount / 2) * elapsedMilliseconds) * 10, 1);
+        ((CPUResult)Result).System = value;
+        return value;
     }
+
 }
