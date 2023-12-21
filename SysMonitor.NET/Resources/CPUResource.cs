561528d76d402393d55aaa6d54fce037d72c4680
drew-chase <dcmanproductions@gmail.com>
2023-12-21 09:58:59 -0500

    Add CPUResource class for monitoring CPU usage

---
+++ SysMonitor.NET/Resources/CPUResource.cs
@@ -0,0 +1,50 @@
+﻿using System.Diagnostics;
+
+namespace SysMonitor.NET.Resources;
+
+public class CPUResource(string name, double maxValue, double minValue) : ResourceItemBase<double>(name, maxValue, minValue)
+{
+    public override double GetApplicationUsage()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override double GetSystemUsage()
+    {
+        long startTime = Environment.TickCount64;
+        Process[] startProcesses = Process.GetProcesses();
+        while ((Environment.TickCount64 - startTime) < 100) // Wait 100ms
+        {
+        }
+
+        Process[] endProcesses = Process.GetProcesses();
+        double startTotalProcessTime = startProcesses.Sum(p =>
+        {
+            try
+            {
+                return p.TotalProcessorTime.TotalMilliseconds;
+            }
+            catch (UnauthorizedAccessException)
+            {
+                return 0;
+            }
+        });
+        double endTotalProcessTime = endProcesses.Sum(p =>
+        {
+            try
+            {
+                return p.TotalProcessorTime.TotalMilliseconds;
+            }
+            catch (UnauthorizedAccessException)
+            {
+                return 0;
+            }
+        });
+        double totalProcessTime = endTotalProcessTime - startTotalProcessTime;
+        double totalSystemTime = Environment.TickCount64 - startTime;
+        double timePerCore = totalProcessTime / Environment.ProcessorCount * totalSystemTime;
+        double adjustedTimePerCore = timePerCore * 10;
+        return Math.Min(adjustedTimePerCore, 1);
+
+    }
+}
