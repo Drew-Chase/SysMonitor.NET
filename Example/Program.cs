76258577094326efcf0a825a2a4a12ac336b9f43
drew-chase <dcmanproductions@gmail.com>
2023-12-21 16:18:25 -0500

    Refactor program to include CPU, RAM, and Disk monitoring

--- Example/Program.cs
+++ Example/Program.cs
@@ -1,5 +1,7 @@
-﻿using SysMonitor.NET;
-using SysMonitor.NET.Resources;
+﻿using Chase.CommonLib.Math;
+using Hardware.Info;
+using SysMonitor.NET;
+using System.Diagnostics;
 
 namespace Example;
 
@@ -7,11 +9,35 @@ internal class Program
 {
     static void Main()
     {
-        ResourceMonitor monitor = ResourceMonitor.Default;
+        ResourceMonitor monitor = new(TimeSpan.FromSeconds(1), EResourceType.RAM | EResourceType.DISK);
         monitor.OnUpdate += (sender, data) =>
         {
-            Console.WriteLine(data.ToString());
+            if (data.CPU != null)
+            {
+                Console.WriteLine($"CPU: \n" +
+                    $"\tSystem - {data.CPU?.System:P2}\n" +
+                    $"\tApplication - {data.CPU?.Application:P2}");
+            }
+            if (data.RAM != null)
+            {
+                Console.WriteLine($"RAM: \n" +
+                    $"\tSystem - {AdvancedFileInfo.SizeToString((long)(data.RAM?.System ?? 0))} / {AdvancedFileInfo.SizeToString((long)(data.RAM?.Max ?? 0))} ({(data.RAM?.System / (double)(data.RAM?.Max ?? 0)):P2})\n" +
+                    $"\tApplication - {AdvancedFileInfo.SizeToString((long)(data.RAM?.Application ?? 0))} / {AdvancedFileInfo.SizeToString((long)(data.RAM?.Max ?? 0))} ({(data.RAM?.Application / (double)(data.RAM?.Max ?? 0)):P2})");
+            }
+            if (data.Disk != null)
+            {
+                Console.WriteLine($"Disk: \n" +
+                    $"\tSystem - r:{AdvancedFileInfo.SizeToString((long)(data.Disk?.Read ?? 0))}/s w:{AdvancedFileInfo.SizeToString((long)(data.Disk?.Write ?? 0))}/s");
+            }
         };
+
         monitor.Start();
+
+        //// END PROGRAM!
+        Console.ReadLine();
+        monitor.Stop();
+
+        return;
+
     }
 }
