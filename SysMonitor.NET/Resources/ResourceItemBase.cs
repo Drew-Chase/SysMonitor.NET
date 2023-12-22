3bc06e65915be6b4c6116506ac3ae7917d7bb74d
drew-chase <dcmanproductions@gmail.com>
2023-12-22 10:27:50 -0500

    Refactor ResourceItemBase class

--- SysMonitor.NET/Resources/ResourceItemBase.cs
+++ SysMonitor.NET/Resources/ResourceItemBase.cs
@@ -7,36 +7,47 @@ public abstract class ResourceItemBase<T>
 {
     [JsonIgnore]
     public IResourceResult? Result { get; protected set; }
+
     [JsonProperty("name")]
     public string Name { get; protected set; }
+
     [JsonProperty("max")]
     public T MaxValue { get; protected set; }
+
     [JsonProperty("min")]
     public T MinValue { get; protected set; }
+
     protected ResourceItemBase(string name, T maxValue, T minValue)
     {
         Name = name;
         MaxValue = maxValue;
         MinValue = minValue;
     }
+
     public void Update()
     {
         GetApplicationUsage();
         GetSystemUsage();
     }
+
     public async Task UpdateAsync()
     {
         await Task.WhenAll(GetApplicationUsageAsync(), GetSystemUsageAsync());
     }
+
     public async Task<T> GetSystemUsageAsync() => await Task.Run(GetSystemUsage);
+
     public async Task<T> GetApplicationUsageAsync() => await Task.Run(GetApplicationUsage);
+
     public abstract T GetSystemUsage();
+
     public abstract T GetApplicationUsage();
 
     public JObject ToJson()
     {
         return JObject.FromObject(this);
     }
+
     public override string ToString()
     {
         return JsonConvert.SerializeObject(ToJson());
