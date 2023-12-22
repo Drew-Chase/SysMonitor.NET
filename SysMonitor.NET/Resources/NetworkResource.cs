015f17f97661f071c95937c90558e84ce51f0777
drew-chase <dcmanproductions@gmail.com>
2023-12-22 10:11:54 -0500

    Add network monitoring functionality

--- SysMonitor.NET/Resources/NetworkResource.cs
+++ SysMonitor.NET/Resources/NetworkResource.cs
@@ -1,18 +1,96 @@
-﻿namespace SysMonitor.NET.Resources;
+﻿using Newtonsoft.Json.Linq;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Runtime.Versioning;
+using System.Text.RegularExpressions;
+
+namespace SysMonitor.NET.Resources;
 
 public class NetworkResource : ResourceItemBase<RWData>
 {
+    PerformanceCounter[] sent;
+    PerformanceCounter[] received;
     public NetworkResource() : base("Networking", new(ulong.MaxValue, ulong.MaxValue), new(0, 0))
     {
+        if (OperatingSystem.IsWindows())
+        {
+            PerformanceCounterCategory category = new("Network Interface");
+            sent = category.GetInstanceNames().Select(i => new PerformanceCounter("Network Interface", "Bytes Sent/sec", i)).ToArray();
+            received = category.GetInstanceNames().Select(i => new PerformanceCounter("Network Interface", "Bytes Received/sec", i)).ToArray();
+        }
     }
 
     public override RWData GetApplicationUsage()
     {
-        throw new NotImplementedException();
+        return new(0, 0);
     }
 
     public override RWData GetSystemUsage()
     {
-        throw new NotImplementedException();
+        RWData data = new(0, 0);
+
+        if (OperatingSystem.IsWindows())
+        {
+            ulong r = 0;
+            ulong s = 0;
+
+            for (int i = 0; i < Math.Min(sent.Length, received.Length); i++)
+            {
+                s += (ulong)sent[i].NextValue();
+                r += (ulong)received[i].NextValue();
+            }
+
+            data = new(r, s);
+        }
+        else if (OperatingSystem.IsLinux())
+        {
+            string command = "ip -s -link -j";
+            ProcessStartInfo processInfo = new("bash", $"-c \"{command}\"")
+            {
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
+
+            Process process = new() { StartInfo = processInfo };
+            string content = "";
+            process.OutputDataReceived += (sender, data) =>
+            {
+                if (!string.IsNullOrWhiteSpace(data.Data))
+                {
+                    content += data.Data;
+                }
+            };
+            process.Start();
+            process.BeginOutputReadLine();
+            process.BeginErrorReadLine();
+            process.WaitForExit();
+
+            if (process.ExitCode != 0 || string.IsNullOrWhiteSpace(content))
+            {
+                return (RWData)(Result = data);
+            }
+
+            foreach (JObject json in JArray.Parse(content).Cast<JObject>())
+            {
+                if (json["flags"] is JArray flags)
+                {
+                    if (flags.Any(f => f.ToString().Equals("LOOPBACK")))
+                    {
+                        continue;
+                    }
+                }
+
+                if (json["stats"] is JObject stats)
+                {
+                    data.Read += stats["rx"]?["bytes"]?.ToObject<ulong>() ?? 0;
+                    data.Write += stats["tx"]?["bytes"]?.ToObject<ulong>() ?? 0;
+                }
+            }
+        }
+        return (RWData)(Result = data);
     }
+
 }
