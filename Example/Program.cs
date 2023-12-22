f450b14c53ded232304b62eae593b79c35d41979
drew-chase <dcmanproductions@gmail.com>
2023-12-22 10:11:41 -0500

    Add networking information to resource monitor

--- Example/Program.cs
+++ Example/Program.cs
@@ -9,9 +9,10 @@ internal class Program
 {
     static void Main()
     {
-        ResourceMonitor monitor = new(TimeSpan.FromSeconds(1), EResourceType.RAM | EResourceType.DISK);
+        ResourceMonitor monitor = ResourceMonitor.Default;
         monitor.OnUpdate += (sender, data) =>
         {
+            Console.Clear();
             if (data.CPU != null)
             {
                 Console.WriteLine($"CPU: \n" +
@@ -28,6 +29,11 @@ internal class Program
             {
                 Console.WriteLine($"Disk: \n" +
                     $"\tSystem - r:{AdvancedFileInfo.SizeToString((long)(data.Disk?.Read ?? 0))}/s w:{AdvancedFileInfo.SizeToString((long)(data.Disk?.Write ?? 0))}/s");
+            }
+            if (data.Networking != null)
+            {
+                Console.WriteLine($"Networking: \n" +
+                    $"\tSystem - r:{AdvancedFileInfo.SizeToString((long)(data.Networking?.Read ?? 0))}/s w:{AdvancedFileInfo.SizeToString((long)(data.Networking?.Write ?? 0))}/s");
             }
         };
 
